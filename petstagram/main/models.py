27129e73a609b54dfea24b8eb662ec36ad986cf3
Георги Тучев <g.tuchev@gmail.com>
2022-04-18 16:59:02 +0300

    Initial commit

--- petstagram/main/models.py
+++ petstagram/main/models.py
@@ -1,7 +1,9 @@
+import datetime
+
 from django.core.validators import MinLengthValidator
 from django.db import models
 
-from petstagram.main.validators import only_letters_validator
+from petstagram.main.validators import validate_only_letters, validate_file_max_size_in_mb
 
 
 class Profile(models.Model):
@@ -26,7 +28,7 @@ class Profile(models.Model):
         max_length=FIRST_NAME_MAX_LENGTH,
         validators=(
             MinLengthValidator(FIRST_NAME_MIN_LENGTH),
-            only_letters_validator,
+            validate_only_letters,
         )
     )
 
@@ -34,7 +36,7 @@ class Profile(models.Model):
         max_length=LAST_NAME_MAX_LENGTH,
         validators=(
             MinLengthValidator(LAST_NAME_MIN_LENGTH),
-            only_letters_validator,
+            validate_only_letters,
             # always_valid('asd'),
         )
     )
@@ -43,7 +45,7 @@ class Profile(models.Model):
 
     date_of_birth = models.DateField(
         null=True,
-        blanl=True,
+        blank=True,
     )
 
     description = models.TextField(
@@ -61,4 +63,84 @@ class Profile(models.Model):
         choices=GENDERS,
         null=True,
         blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Pet(models.Model):
+    # Constants
+    CAT = 'Cat'
+    DOG = 'Dog'
+    BUNNY = 'Bunny'
+    PARROT = 'Parrot'
+    FISH = 'Fish'
+    OTHER = 'Other'
+
+    TYPES = [(x, x) for x in (CAT, DOG, BUNNY, PARROT, FISH, OTHER)]
+    #     TYPES = ((x, x) for x in (CAT, DOG, BUNNY, PARROT, FISH, OTHER)) --> not a tuple comprehension, generator
+    NAME_MAX_LENGTH = 30
+
+    # Fields(Columns)
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+    )
+
+    type = models.CharField(
+        max_length=max(len(x) for (x, _) in TYPES),
+        choices=TYPES,
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    # One-to-one relations
+
+    # One-to-many relations
+    user_profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+
+    # Many-to-many relations
+
+    # Properties
+    @property
+    def age(self):
+        return datetime.datetime.now().year - self.date_of_birth.year
+
+    # Methods
+
+    # Dunder methods
+
+    # Meta
+    class Meta:
+        unique_together = ('user_profile', 'name')
+
+
+class PetPhoto(models.Model):
+    photo = models.ImageField(
+        validators=(
+            # validate_file_max_size_in_mb(5),
+        )
+    )
+    tagged_pets = models.ManyToManyField(
+        Pet,
+        # Validate at least 1 pet
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    publication_date = models.DateTimeField(
+        auto_created=True,
+    )
+
+    likes = models.IntegerField(
+        default=0,
     )
